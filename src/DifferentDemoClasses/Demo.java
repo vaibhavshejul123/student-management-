9db528822ea7b111491350cd0067cd2a0873ca65
VaibhavShejul123 <shejulv12@gmail.com>
2023-04-18 22:53:06 +0530

    First Commit By Vaibhav

---
+++ src/DifferentDemoClasses/Demo.java
@@ -0,0 +1,10 @@
+package DifferentDemoClasses;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sheju
+ *
+ */
+module FirstJavaProject {
+}
